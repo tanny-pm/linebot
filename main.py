34fd6b0e1cff1e9c19d87b33361e7bca5c17dcaa
Tanny <ryosuke.tanimura+dev@gmail.com>
2023-03-11 15:38:22 +0900

    Add: line botのメイン機能を実装

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from fastapi import FastAPI
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI, Header, Request
+from starlette.exceptions import HTTPException
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+load_dotenv()
 
 app = FastAPI()
+line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
 
 
 @app.get("/")
@@ -11,3 +23,25 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.post("/callback")
+async def callback(request: Request, x_line_signature=Header(None)):
+    body = await request.body()
+
+    try:
+        handler.handle(body.decode("utf-8"), x_line_signature)
+    except InvalidSignatureError:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid signature.",
+        )
+
+    return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text=event.message.text)
+    )
